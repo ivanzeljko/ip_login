370b4c82ccf1956ae280853104cd813e215c7ed3
bjaxelsen <bjorn.axelsen@fagkommunikation.dk>
2017-10-05 13:03:33 +0200

    Changing session limit and IP login modules do to problems with re login

--- js/ip_login.js
+++ js/ip_login.js
@@ -7,36 +7,43 @@
  * We want this to run as fast as possible, hence we use pure JS and do not wait for document to be ready,
  */
 
-function ipReadCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return -1;
+function ipLoginReadCookie(name) {
+  'use strict';
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {c = c.substring(1, c.length);}
+    if (c.indexOf(nameEQ) == 0) {return c.substring(nameEQ.length, c.length);}
+  }
+  return -1;
 }
 
-var ipLoginCookie = ipReadCookie("ipLogin");
+document.addEventListener('DOMContentLoaded', function (event) {
+  'use strict';
 
-now = new Date().getTime();
-if (
+  var ipLoginCookie = ipLoginReadCookie('ipLogin');
+  var ipLoginNow = new Date().getTime();
+
+  if (
     ipLoginCookie != 1 && // If not already logged in
-    navigator.cookieEnabled && // and cookies must be enabled
-    (ipLoginCookie == -1 || ipLoginCookie < (now - 10000)) // if not tried to log in or more than 10 seconds ago
-) {
-    // Set cookie with timestamp to avoid any more requests for now
-    document.cookie = "ipLogin=" + now + ";path=/";
+        navigator.cookieEnabled && // and cookies must be enabled
+        (ipLoginCookie == -1 || ipLoginCookie < (ipLoginNow - 10000)) // if not tried to log in or more than 10 seconds ago
+  ) {
+    // Set cookie with timestamp to avoid any more requests for ipLoginNow
+    document.cookie = 'ipLogin=' + ipLoginNow + ';path=/';
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', ipLogin.callback, true);
+    xhr.open('GET', ipLogin.query, true);
+    xhr.withCredentials = true;
     xhr.onload = function (e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            if (xhr.responseText == '1') {
-                document.cookie = "ipLogin=1;path=/";
-                location.reload();
-            }
+      'use strict';
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.responseText == '1') {
+          document.cookie = 'ipLogin=1;path=/';
+          window.location.href = ipLogin.dologin;
         }
-    }
+      }
+    };
     xhr.send();
-}
+  }
+});
--- src/LoginController.php
+++ src/LoginController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\user\Entity\User;
+use Drupal\Core\Url;
 
 class LoginController extends ControllerBase {
   
@@ -15,14 +16,74 @@ class LoginController extends ControllerBase {
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    *   A JSON response containing the autocomplete suggestions for Views tags.
    */
-  public function loginRequest(Request $request) {
-    $match = FALSE;
+  public function loginQuery(Request $request) {
+    if ($uid = $this->checkIpLoginExists($request)) {
+      \Drupal::logger('ip_login')->notice('User @uid matched for IP login from @ip (ajax call).',
+        [
+          '@uid' => $uid,
+          '@ip' => $request->getClientIp(),
+        ]);
+      return new JsonResponse(1);
+    }
+    else {
+      return new JsonResponse(0);
+    }
+  }
 
-    // Make sure we are not already logged in
-    if (FALSE && \Drupal::currentUser()->id()) {
-      return new JsonResponse(-1);
+  /**
+   * Menu callback for IP based login: do the actual login
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   A JSON response containing the autocomplete suggestions for Views tags.
+   */
+  public function loginProcess(Request $request) {
+    $uid = $this->checkIpLoginExists($request);
+
+    if (empty($uid)) {
+      \Drupal::logger('ip_login')->warning('IP login processing accessed without any matches from @ip.',
+        [
+          '@ip' => $request->getClientIp(),
+        ]);
+
+    }
+    else {
+      $user = User::load($uid);
+
+      \Drupal::logger('ip_login')
+        ->notice('Logging in user @uid through IP login from @ip.',
+          [
+            '@uid' => $uid,
+            '@ip' => $request->getClientIp(),
+          ]);
+
+      user_login_finalize($user);
+
+      drupal_set_message(t('You have been logged in automatically using IP login.'));
     }
 
+    $destination = Url::fromUserInput(\Drupal::destination()->get());
+
+    if ($destination->isRouted()) {
+      // Valid internal path.
+      return $this->redirect(
+        $destination->getRouteName(),
+        $destination->getRouteParameters()
+      );
+    }
+    else {
+      return $this->redirect('<front>');
+    }
+  }
+
+  /**
+   * Lookup if current request IP matches an IP login
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request
+   * @return int|boolean uid
+   *   The user ID or FALSE if not
+   */
+  public static function checkIpLoginExists(Request $request) {
     $field_name = \Drupal::config('ip_login.settings')->get('address_field');
 
     $ip = inet_pton($request->getClientIp());
@@ -34,22 +95,9 @@ class LoginController extends ControllerBase {
     $uids = $query->execute();
 
     if (empty($uids)) {
-      return new JsonResponse(0);
+      return FALSE;
     }
 
-    $uid = current($uids);
-
-    $user = User::load($uid);
-    user_login_finalize($user);
-
-    \Drupal::logger('ip_login')->notice('User @uid matched for IP login from @ip.',
-      [
-        '@uid' => $uid,
-        '@ip' => $request->getClientIp(),
-      ]);
-
-    drupal_set_message(t('You have been logged in automatically using IP login.'));
-
-    return new JsonResponse(1);
+    return current($uids);
   }
 }
